556e7c8ba6c19665410120b223d8e7bd4e522fd6
Nicolas le plus beau <nicolas.trenchs@etu.unilim.fr>
2024-10-25 15:02:15 +0200

    ajout category dans bdd

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category; // Assure-toi d'importer le modèle
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +12,10 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = ['Soul', 'Ambient', 'Pop', 'Rap', 'Funk', 'Rock', 'Reggae / Dub', 'Techno', 'Electro'];
+
+        foreach ($categories as $name) {
+            Category::create(['name' => $name]); // Insérer la catégorie
+        }
     }
 }
